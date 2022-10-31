aa95faa40846d7e1c584661aa78df506183b6e59
spalpeen <oxybimboo@gmail.com>
2022-10-31 16:02:58 +0200

    App logging

--- chatapp/client.py
+++ chatapp/client.py
@@ -3,9 +3,14 @@ import sys
 from socket import socket, AF_INET, SOCK_STREAM
 from common.utils import send_message, get_message
 from common.variables import ACTION, USER, TIME, ACCOUNT_NAME, PRESENCE, DEFAULT_PORT
+import logging
+import logs.client_log_config
+
+logger = logging.getLogger('chatapp.client')
 
 
 def generate_presence(username='Guest'):
+    logger.debug('Generating presence')
     return {
         ACTION: PRESENCE,
         TIME: time.time(),
@@ -19,23 +24,31 @@ def main():
     try:
         address = sys.argv[1]
     except IndexError:
-        print('Необходимо указать ip сервера')
+        logger.critical('You need to specify the IP of the server.')
+        sys.exit(1)
 
     try:
-        port = int(sys.argv[2]) if len(sys.argv) > 2 else DEFAULT_PORT
+        port = int(sys.argv[2]) if len(sys.argv) == 3 else DEFAULT_PORT
         if port < 1024 or port > 65535:
             raise ValueError
     except IndexError:
-        print("<script's name> <address> <port>")
+        logger.critical('You need to specify the port of the server.')
         sys.exit(1)
     except ValueError:
-        print('1024 < port < 65535')
+        logger.critical('The input number for the port must be in the range from 1024 to 65535.')
         sys.exit(1)
 
     client_socket = socket(AF_INET, SOCK_STREAM)
-    client_socket.connect((address, port))
+    try:
+        client_socket.connect((address, port))
+        logger.info('Successful connection to the server.')
+    except ConnectionRefusedError:
+        logger.error('Invalid ip or port specified to connect to the server.')
+        sys.exit(1)
     send_message(client_socket, generate_presence())
+    logger.info('The message is sent')
     get_message(client_socket)
+    logger.info('The message is received')
     client_socket.close()
 
 
--- chatapp/server.py
+++ chatapp/server.py
@@ -1,11 +1,16 @@
-import json
 import sys
 from socket import socket, AF_INET, SOCK_STREAM
 from common.utils import send_message, get_message
-from common.variables import RESPONSE, ACTION, PRESENCE, TIME, USER, ACCOUNT_NAME, ALERT, DEFAULT_PORT
+from common.variables import RESPONSE, ACTION, PRESENCE, TIME, USER, ACCOUNT_NAME, ALERT, DEFAULT_PORT, MAX_CONNECTION
+import logging
+import logs.server_log_config
+
+logger = logging.getLogger('chatapp.server')
 
 
 def generate_response(message):
+    logger.debug('Generating response')
+    logger.info('200/OK')
     if (message.get(ACTION) is not None and message[ACTION] == PRESENCE and message.get(TIME) is not None
             and message.get(USER) is not None and message[USER][ACCOUNT_NAME] == 'Guest'):
         return {
@@ -13,6 +18,7 @@ def generate_response(message):
             ALERT: "OK"
         }
     else:
+        logger.info('400/BAD REQUEST')
         return {
             RESPONSE: 400,
             ALERT: "BAD REQUEST"
@@ -21,33 +27,35 @@ def generate_response(message):
 
 def main():
     try:
-        port = int(sys.argv[sys.argv.index('-p') + 1]) if '-p' in sys.argv else DEFAULT_PORT
-        if port < 1024 or port > 65535:
-            raise ValueError
+        address = sys.argv[sys.argv.index('-a') + 1] if '-a' in sys.argv else ''
     except IndexError:
-        print("<script's name> -a <address> -p <port>")
-        sys.exit(1)
-    except ValueError:
-        print('1024 < port < 65535')
+        logger.critical('Incorrect IP specified during server startup.')
         sys.exit(1)
 
     try:
-        address = sys.argv[sys.argv.index('-a') + 1] if '-a' in sys.argv else ''
+        port = int(sys.argv[sys.argv.index('-p') + 1]) if '-p' in sys.argv else DEFAULT_PORT
+        if port < 1024 or port > 65535:
+            raise ValueError
+    except ValueError:
+        logger.critical('The input number for the port must be in the range from 1024 to 65535.')
+        sys.exit(1)
     except IndexError:
-        print("<script's name> -a <address> -p <port>")
+        logger.critical('When starting the server, the port was not found in the parameters.')
         sys.exit(1)
 
     server_socket = socket(AF_INET, SOCK_STREAM)
     server_socket.bind((address, port))
-    server_socket.listen(5)
-
+    server_socket.listen(MAX_CONNECTION)
+    logger.debug('The socket is ready to receive messages')
     while True:
         client, address = server_socket.accept()
         try:
             send_message(client, generate_response(get_message(client)))
+            logger.info('The message is received')
+            logger.info('The response is sent.')
             client.close()
-        except (ValueError, json.JSONDecodeError):
-            print('Error message')
+        except ValueError:
+            logger.error('Message type error.')
             client.close()
 
 
